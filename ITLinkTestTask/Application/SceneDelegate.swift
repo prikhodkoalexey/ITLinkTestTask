acaec843a8e5ebe1488aad3f87818f73e2934806
Aleksei Prikhodko <prikhodko.alexey.v@gmail.com>
2025-10-19 21:13:38 +0300

    Очистил шаблон и добавил каркас приложения

---
+++ ITLinkTestTask/Application/SceneDelegate.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let scene = scene as? UIWindowScene else { return }
+        let window = UIWindow(windowScene: scene)
+        window.rootViewController = RootViewController()
+        window.makeKeyAndVisible()
+        self.window = window
+    }
+}
